5344093e5bd0a92d1126024a5e0cfe08eac0a5f8
evoney <evoney.mendonca@icomp.ufam.edu.br>
2021-04-24 11:40:32 -0400

    bug: email template renewed

--- src/controllers/sendMailController.ts
+++ src/controllers/sendMailController.ts
@@ -3,18 +3,17 @@ import sendMailService from '../services/sendMailService';
 import path from 'path';
 
 class SendMailController {
-   async execute(request: Request, response: Response) {
+  async execute(request: Request, response: Response) {
    
   const template = path.resolve(__dirname, "..", "views", "email", "template.hbs");
   const variables = {
     name: "name",
     title: "title",
-    link: process.env.URL_MAIL,
-}
+  }
     const { email } = request.body;
     await sendMailService.execute(email, "teste", variables, template);
 
-
+    return response.json(email);
    }
 }
 
