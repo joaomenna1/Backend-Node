1d4735916063bdff8100d6a07458e36c77a947eb
joaomenna1 <joaomenna1@gmail.com>
2021-07-15 01:14:47 -0300

    fix errors modules

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import AppointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
-import UsersRouter from '@modules/users/infra/http/routes/users.routes';
-import SessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
-import PasswordRouter from '@modules/users/infra/http/routes/password.routes';
-import profileRouter from '@modules/users/infra/http/routes/profile.routes';
-import providerRouter from '@modules/appointments/infra/http/routes/providers.routes';
+import AppointmentsRouter from '../../../../modules/appointments/infra/http/routes/appointments.routes';
+import UsersRouter from '../../../../modules/users/infra/http/routes/users.routes';
+import SessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
+import PasswordRouter from '../../../../modules/users/infra/http/routes/password.routes';
+import profileRouter from '../../../../modules/users/infra/http/routes/profile.routes';
+import providerRouter from '../../../../modules/appointments/infra/http/routes/providers.routes';
 
 const routes = Router();
 
